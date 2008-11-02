6e13ccb9890d215935bef066bb3447fa5d16a2b3
Postmodern <postmodern.mod3@gmail.com>
2008-11-02 00:41:53 +0000

    * How'd this file get deleted.

---
+++ static/course/scripts/results.js
@@ -0,0 +1,25 @@
+function getResults(url)
+{
+  $.getJSON(url,function(results) {
+    $("body").append("<a href='#' class='show'>Expected Results</a><div id='expected'><ul id='followed'></ul><ul id='ignored'></ul></div>");
+
+    var expected = $("#expected");
+
+    $("a.show").click(function() {
+      expected.slideToggle("normal");
+    });
+
+    var ignored = $("#ignored");
+    var followed = $("#followed");
+
+    expected.hide();
+
+    $.each(results.followed,function(link,example) {
+      $("<li></li>").appendTo(followed).text(example);
+    });
+
+    $.each(results.ignored function(link,example) {
+      $("<li></li>").appendTo(ignored).text(example);
+    });
+  });
+});
