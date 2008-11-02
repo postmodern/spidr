b20bb56336ae918fd8e93ea0db9436a291c7dc13
Postmodern <postmodern.mod3@gmail.com>
2008-11-02 01:14:41 +0000

    * Added more functions.

--- static/course/scripts/course.js
+++ static/course/scripts/course.js
@@ -1,20 +1,52 @@
+var sections = {
+  empty: {
+    name: "Empty Links",
+    results_url: "/course/empty/results.json"
+  },
+
+  javascript: {
+    name: "JavaScript Links",
+    results_url: "/course/javascript/results.json"
+  },
+
+  loop: {
+    name: "Looping Links",
+    results_url: "/course/loop/results.json"
+  },
+
+  relative: {
+    name: "Relative Links",
+    results_url: "/course/relative/results.json"
+  }
+};
+
 function fail() { window.location = "/course/fail.html"; }
 
-function getSectionResults(section)
+function addResults()
 {
-  $.getJSON("/course/" + section + "/results.json",function(results) {
-    $("body").append("<a href='#' class='show'>Expected Results</a><div id='expected'><ul id='followed'></ul><ul id='ignored'></ul></div>");
+  $("body").append("<a href='#' class='results show_results'>Show Expected Results</a><a href='#' class='results hide_results'>Hide Expected Results</a><div id='expected'><ul id='followed'></ul><ul id='ignored'></ul></div>");
 
-    var expected = $("#expected");
+  var expected = $("#expected");
 
-    $("a.show").click(function() {
-      expected.slideToggle("normal");
-    });
+  expected.hide();
 
+  $("a.hide_results").hide();
+
+  $("a.results").click(function() {
+    $("a.show_results").toggleShow();
+    $("a.hide_results").toggleShow();
+
+    expected.slideToggle("normal");
+  });
+
+  return expected;
+}
+
+function getSectionResults(name)
+{
+  $.getJSON(sections[name]['results_url'],function(results) {
     var ignored = $("#ignored");
     var followed = $("#followed");
-
-    expected.hide();
 
     $.each(results.followed,function(link,example) {
       $("<li></li>").appendTo(followed).text(example);
@@ -23,5 +55,20 @@ function getSectionResults(section)
     $.each(results.ignored,function(link,example) {
       $("<li></li>").appendTo(ignored).text(example);
     });
+  });
+}
+
+function addSectionResults(name)
+{
+  addResults();
+  getSectionResults(name);
+}
+
+function addAllResults()
+{
+  addResults();
+
+  $.each(sections,function(name,settings) {
+    getSectionResults(name);
   });
 }
