285b19985067ec60e13eef4e0a0d25a96072816f
Postmodern <postmodern.mod3@gmail.com>
2008-11-02 06:14:08 +0000

    * Generate absolute URLs in the JSON specs file.

--- tasks/course.rb
+++ tasks/course.rb
@@ -2,6 +2,8 @@ require 'hpricot'
 require 'json'
 
 namespace :course do
+  COURSE_URL = URI('http://spidr.rubyforge.org/course/')
+
   COURSE_DIR = File.expand_path(File.join(File.dirname(__FILE__),'..','static','course'))
 
   desc "Build the JSON spec file for the course"
@@ -11,13 +13,14 @@ namespace :course do
 
       Dir[File.join(COURSE_DIR,'**','*.html')].each do |page|
         doc = Hpricot(open(page))
+        page_url = COURSE_URL.merge(page.sub(COURSE_DIR,''))
 
         link_to_spec = lambda { |container|
           link = container.at('a')
 
           {
             :message => link.inner_text,
-            :link => link['href'],
+            :link => page_url.merge(URI.encode(link['href'].to_s)),
             :example => link.to_html
           }
         }
