ed6babdb02c09f29f6b44802de5662c9cabcdcfc
postmodern <postmodern.mod3@gmail.com>
2009-05-24 19:13:32 -0700

    Added specs for history, queue, pause! and continue! methods.

--- spec/agent_spec.rb
+++ spec/agent_spec.rb
@@ -9,4 +9,39 @@ describe Agent do
   before(:all) do
     @agent = run_course
   end
+
+  it "should provide the history" do
+    @agent.history.should_not be_empty
+  end
+
+  it "should provide the queue" do
+    @agent.queue.should be_empty
+  end
+
+  it "should be able to pause spidering" do
+    count = 0
+    agent = Agent.host('spidr.rubyforge.org') do |spider|
+      spider.every_page do |page|
+        count += 1
+        spider.pause! if count >= 2
+      end
+    end
+
+    agent.should be_paused
+    agent.history.length.should == 2
+  end
+
+  it "should be able to continue spidering after being paused" do
+    agent = Agent.new do |spider|
+      spider.enqueue('http://spidr.rubyforge.org/')
+      spider.every_page do |page|
+        spider.pause!
+      end
+    end
+
+    agent.pause!
+    agent.continue!
+
+    agent.visited?('http://spidr.rubyforge.org/').should == true
+  end
 end
