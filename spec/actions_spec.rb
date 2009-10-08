1d6525483334124e9dfe09f1346d65f4589c90f6
postmodern <postmodern.mod3@gmail.com>
2009-10-08 15:58:20 -0700

    Added more specs for Spidr::Actions.

--- spec/actions_spec.rb
+++ spec/actions_spec.rb
@@ -4,6 +4,10 @@ require 'spidr/agent'
 require 'spec_helper'
 
 describe Spidr::Actions do
+  before(:all) do
+    @url = URI('http://spidr.rubyforge.org/')
+  end
+
   it "should be able to pause spidering" do
     count = 0
     agent = Agent.host('spidr.rubyforge.org') do |spider|
@@ -19,14 +23,39 @@ describe Spidr::Actions do
 
   it "should be able to continue spidering after being paused" do
     agent = Agent.new do |spider|
-      spider.enqueue('http://spidr.rubyforge.org/')
       spider.every_page do |page|
         spider.pause!
       end
     end
 
+    agent.enqueue(@url)
     agent.continue!
 
-    agent.visited?('http://spidr.rubyforge.org/').should == true
+    agent.visited?(@url).should == true
+  end
+
+  it "should allow skipping of enqueued links" do
+    agent = Agent.new do |spider|
+      spider.every_url do |url|
+        spider.skip_link!
+      end
+    end
+
+    agent.enqueue(@url)
+
+    agent.queue.should be_empty
+  end
+
+  it "should allow skipping of visited pages" do
+    agent = Agent.new do |spider|
+      spider.every_page do |url|
+        spider.skip_page!
+      end
+    end
+
+    agent.visit_page(@url)
+
+    agent.history.should == Set[@url]
+    agent.queue.should be_empty
   end
 end
