9ea68a94dafdd5087d9f9f60decd587639a11483
postmodern <postmodern.mod3@gmail.com>
2009-10-08 23:16:59 -0700

    Added YARD tags to Spidr::Agent.

--- lib/spidr/agent.rb
+++ lib/spidr/agent.rb
@@ -36,18 +36,47 @@ module Spidr
     attr_reader :queue
 
     #
-    # Creates a new Agent object with the given _options_ and _block_.
-    # If a _block_ is given, it will be passed the newly created
-    # Agent object.
+    # Creates a new Agent object.
     #
-    # _options_ may contain the following keys:
-    # <tt>:proxy</tt>:: The proxy to use while spidering.
-    # <tt>:user_agent</tt>:: The User-Agent string to send.
-    # <tt>:referer</tt>:: The referer URL to send.
-    # <tt>:delay</tt>:: Duration in seconds to pause between spidering each
-    #                   link. Defaults to 0.
-    # <tt>:queue</tt>:: An initial queue of URLs to visit.
-    # <tt>:history</tt>:: An initial list of visited URLs.
+    # @param [Hash] options
+    #   Additional options
+    #
+    # @option options [Hash] :proxy (Spidr.proxy)
+    #   The proxy information to use.
+    #
+    # @option :proxy [String] :host
+    #   The host the proxy is running on.
+    #
+    # @option :proxy [Integer] :port
+    #   The port the proxy is running on.
+    #
+    # @option :proxy [String] :user
+    #   The user to authenticate as with the proxy.
+    #
+    # @option :proxy [String] :password
+    #   The password to authenticate with.
+    #
+    # @option options [String] :user_agent (Spidr.user_agent)
+    #   The User-Agent string to send with each requests.
+    #
+    # @option options [String] :referer
+    #   The Referer URL to send with each request.
+    #
+    # @option options [Integer] :delay (0)
+    #   The number of seconds to pause between each request.
+    #
+    # @option options [Set, Array] :queue
+    #   The initial queue of URLs to visit.
+    #
+    # @option options [Set, Array] :history
+    #   The initial list of visited URLs.
+    #
+    # @yield [agent]
+    #   If a block is given, it will be passed the newly created agent
+    #   for further configuration.
+    #
+    # @yieldparam [Agent] agent
+    #   The newly created agent.
     #
     def initialize(options={},&block)
       @proxy = (options[:proxy] || Spidr.proxy)
@@ -68,10 +97,20 @@ module Spidr
     end
 
     #
-    # Creates a new Agent object with the given _options_ and will begin
-    # spidering at the specified _url_. If a _block_ is given it will be
-    # passed the newly created Agent object, before the agent begins
-    # spidering.
+    # Creates a new agent and begin spidering at the given URL.
+    #
+    # @param [URI::HTTP, String] url
+    #   The URL to start spidering at.
+    #
+    # @param [Hash] options
+    #   Additional options. See {initialize}.
+    #
+    # @yield [agent]
+    #   If a block is given, it will be passed the newly created agent
+    #   before it begins spidering.
+    #
+    # @yieldparam [Agent] agent
+    #   The newly created agent.
     #
     def self.start_at(url,options={},&block)
       self.new(options) do |spider|
@@ -82,10 +121,20 @@ module Spidr
     end
 
     #
-    # Creates a new Agent object with the given _options_ and will begin
-    # spidering the specified host _name_. If a _block_ is given it will be
-    # passed the newly created Agent object, before the agent begins
-    # spidering.
+    # Creates a new agent and spiders the given host.
+    #
+    # @param [String]
+    #   The host-name to spider.
+    #
+    # @param [Hash] options
+    #   Additional options. See {initialize}.
+    #
+    # @yield [agent]
+    #   If a block is given, it will be passed the newly created agent
+    #   before it begins spidering.
+    #
+    # @yieldparam [Agent] agent
+    #   The newly created agent.
     #
     def self.host(name,options={},&block)
       self.new(options.merge(:host => name)) do |spider|
@@ -96,10 +145,20 @@ module Spidr
     end
 
     #
-    # Creates a new Agent object with the given _options_ and will begin
-    # spidering the host of the specified _url_. If a _block_ is given it
-    # will be passed the newly created Agent object, before the agent
-    # begins spidering.
+    # Creates a new agent and spiders the web-site located at the given URL.
+    #
+    # @param [URI::HTTP, String] url
+    #   The web-site to spider.
+    #
+    # @param [Hash] options
+    #   Additional options. See {initialize}.
+    #
+    # @yield [agent]
+    #   If a block is given, it will be passed the newly created agent
+    #   before it begins spidering.
+    #
+    # @yieldparam [Agent] agent
+    #   The newly created agent.
     #
     def self.site(url,options={},&block)
       url = URI(url.to_s)
@@ -122,8 +181,16 @@ module Spidr
     end
 
     #
-    # Start spidering at the specified _url_. If a _block_ is given, it will
-    # be passed every page visited.
+    # Start spidering at the given url.
+    #
+    # @param [URI::HTTP, String] url
+    #   The URL to start spidering at.
+    #
+    # @yield [page]
+    #   If a block is given, it will be passed every page visited.
+    #
+    # @yieldparam [Page] page
+    #   A page which has been visited.
     #
     def start_at(url,&block)
       enqueue(url)
@@ -133,7 +200,13 @@ module Spidr
 
     #
     # Start spidering until the queue becomes empty or the agent is
-    # paused. If a _block_ is given, pass it every visited page.
+    # paused.
+    #
+    # @yield [page]
+    #   If a block is given, it will be passed every page visited.
+    #
+    # @yieldparam [Page] page
+    #   A page which has been visited.
     #
     def run(&block)
       @running = true
@@ -162,15 +235,23 @@ module Spidr
     end
 
     #
-    # Returns +true+ if the agent is running, returns +false+ otherwise.
+    # Determines if the agent is running.
+    #
+    # @return [Boolean]
+    #   Specifies whether the agent is running or stopped.
     #
     def running?
       @running == true
     end
 
     #
-    # Sets the history of links that were previously visited to the
-    # specified _new_history_.
+    # Sets the history of links that were previously visited.
+    #
+    # @param [#each] new_history
+    #   A list of URLs to populate the history with.
+    #
+    # @return [Set<URI::HTTP>]
+    #   The history of the agent.
     #
     # @example
     #   agent.history = ['http://tenderlovemaking.com/2009/05/06/ann-nokogiri-130rc1-has-been-released/']
@@ -192,22 +273,33 @@ module Spidr
     alias visited_urls history
 
     #
-    # Returns the +Array+ of visited links.
+    # Specifies the links which have been visited.
+    #
+    # @return [Array<String>]
+    #   The links which have been visited.
     #
     def visited_links
       @history.map { |url| url.to_s }
     end
 
     #
-    # Return the +Array+ of hosts that were visited.
+    # Specifies all hosts that were visited.
+    #
+    # @return [Array<String>]
+    #   The hosts which have been visited.
     #
     def visited_hosts
       visited_urls.map { |uri| uri.host }.uniq
     end
 
     #
-    # Returns +true+ if the specified _url_ was visited, returns +false+
-    # otherwise.
+    # Determines whether a URL was visited or not.
+    #
+    # @param [URI::HTTP, String] url
+    #   The URL to search for.
+    #
+    # @return [Boolean]
+    #   Specifies whether a URL was visited.
     #
     def visited?(url)
       url = URI(url.to_s) unless url.kind_of?(URI)
@@ -216,8 +308,13 @@ module Spidr
     end
 
     #
-    # Returns +true+ if the specified _url_ was unable to be visited,
-    # returns +false+ otherwise.
+    # Determines whether a given URL could not be visited.
+    #
+    # @param [URI::HTTP, String] url
+    #   The URL to check for failures.
+    #
+    # @return [Boolean]
+    #   Specifies whether the given URL was unable to be visited.
     #
     def failed?(url)
       url = URI(url.to_s) unless url.kind_of?(URI)
@@ -228,7 +325,13 @@ module Spidr
     alias pending_urls queue
 
     #
-    # Sets the queue of links to visit to the specified _new_queue_.
+    # Sets the queue of URLs to visit.
+    #
+    # @param [#each]
+    #   The new list of URLs to visit.
+    #
+    # @return [Array<URI::HTTP>]
+    #   The list of URLs to visit.
     #
     # @example
     #   agent.queue = ['http://www.vimeo.com/', 'http://www.reddit.com/']
@@ -248,17 +351,27 @@ module Spidr
     end
 
     #
-    # Returns +true+ if the specified _url_ is queued for visiting, returns
-    # +false+ otherwise.
+    # Determines whether a given URL has been enqueued.
+    #
+    # @param [URI::HTTP] url
+    #   The URL to search for in the queue.
+    #
+    # @return [Boolean]
+    #   Specifies whether the given URL has been queued for visiting.
     #
     def queued?(url)
       @queue.include?(url)
     end
 
     #
-    # Enqueues the specified _url_ for visiting, only if it passes all the
-    # agent's rules for visiting a given URL. Returns +true+ if the _url_
-    # was successfully enqueued, returns +false+ otherwise.
+    # Enqueues a given URL for visiting, only if it passes all of the
+    # agent's rules for visiting a given URL.
+    #
+    # @param [URI::HTTP, String] url
+    #   The URL to enqueue for visiting.
+    #
+    # @return [Boolean]
+    #   Specifies whether the URL was enqueued, or ignored.
     #
     def enqueue(url)
       link = url.to_s
@@ -288,8 +401,20 @@ module Spidr
     end
 
     #
-    # Creates a new Page object from the specified _url_. If a _block_ is
-    # given, it will be passed the newly created Page object.
+    # Requests and creates a new Page object from a given URL.
+    #
+    # @param [URI::HTTP] url
+    #   The URL to request.
+    #
+    # @yield [page]
+    #   If a block is given, it will be passed the page that represents the
+    #   response.
+    #
+    # @yieldparam [Page] page
+    #   The page for the response.
+    #
+    # @return [Page, nil]
+    #   The page for the response, or +nil+ if the request failed.
     #
     def get_page(url,&block)
       host = url.host
@@ -323,9 +448,21 @@ module Spidr
     end
 
     #
-    # Visits the spedified _url_ and enqueus it's links for visiting. If a
-    # _block_ is given, it will be passed a newly created Page object
-    # for the specified _url_.
+    # Visits a given URL and enqueus the links recovered from the URL,
+    # to be visited later.
+    #
+    # @param [URI::HTTP, String] url
+    #   The URL to visit.
+    #
+    # @yield [page]
+    #   If a block is given, it will be passed the page which was visited.
+    #
+    # @yieldparam [Page] page
+    #   The page which was visited.
+    #
+    # @return [Page, nil]
+    #   The page that was visited. If +nil+ is returned, either the request
+    #   for the page failed, or the page was skipped.
     #
     def visit_page(url,&block)
       url = URI(url.to_s) unless url.kind_of?(URI)
@@ -349,8 +486,11 @@ module Spidr
     end
 
     #
-    # Returns the agent represented as a Hash containing the agents
-    # +history+ and +queue+ information.
+    # Converts the agent to Hash form.
+    #
+    # @return [Hash]
+    #   The agent represented as a Hash containing the +history+ and
+    #   the +queue+ of the agent.
     #
     def to_hash
       {:history => @history, :queue => @queue}
@@ -359,8 +499,23 @@ module Spidr
     protected
 
     #
-    # Returns the Net::HTTP session for the specified _host_ and _port_.
-    # If a block is given, it will be passed the Net::HTTP session object.
+    # Provides an active HTTP session for the given scheme, host
+    # and port.
+    #
+    # @param [String] scheme
+    #   The scheme of the URL, which will be requested later.
+    #
+    # @param [String] host
+    #   The host that the session is needed with.
+    #
+    # @param [Integer] port
+    #   The port that the session is needed for.
+    #
+    # @yield [session]
+    #   If a block is given, it will be passed the active HTTP session.
+    #
+    # @yieldparam [Net::HTTP] session
+    #   The active HTTP session object.
     #
     def get_session(scheme,host,port,&block)
       key = [scheme,host,port]
@@ -387,7 +542,16 @@ module Spidr
     end
 
     #
-    # Destroys a Net::HTTP session for a given _scheme_, _host_, and _port_
+    # Destroys an HTTP session for the given scheme, host and port.
+    #
+    # @param [String] scheme
+    #   The scheme of the URL, which was requested through the session.
+    #
+    # @param [String] host
+    #   The host that the session was connected with.
+    #
+    # @param [Integer] port
+    #   The port that the session was connected to.
     #
     def kill_session(scheme,host,port,&block)
       key = [scheme,host,port]
@@ -407,13 +571,21 @@ module Spidr
     #
     # Dequeues a URL that will later be visited.
     #
+    # @return [URI::HTTP]
+    #   The URL that was at the front of the queue.
+    #
     def dequeue
       @queue.shift
     end
 
     #
-    # Returns +true+ if the specified URL should be visited, returns
-    # +false+ otherwise.
+    # Determines if a given URL should be visited.
+    #
+    # @param [URI::HTTP] url
+    #   The URL in question.
+    #
+    # @return [Boolean]
+    #   Specifies whether the given URL should be visited.
     #
     def visit?(url)
       (!(visited?(url)) &&
@@ -425,7 +597,10 @@ module Spidr
     end
 
     #
-    # Adds the specified _url_ to the failures list.
+    # Adds a given URL to the failures list.
+    #
+    # @param [URI::HTTP] url
+    #   The URL to add to the failures list.
     #
     def failed(url)
       @every_failed_url_blocks.each { |block| block.call(url) }
