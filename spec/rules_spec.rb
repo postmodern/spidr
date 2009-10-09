33cdc4587967135b58403579d22f95e810b59347
postmodern <postmodern.mod3@gmail.com>
2009-10-08 18:02:39 -0700

    Added specs for Spidr::Rules.

---
+++ spec/rules_spec.rb
@@ -0,0 +1,43 @@
+require 'spidr/rules'
+
+require 'spec_helper'
+
+describe Rules do
+  it "should accept data based on acceptance data" do
+    rules = Rules.new(:accept => [1])
+
+    rules.accept?(1).should == true
+  end
+
+  it "should accept data based on acceptance regexps" do
+    rules = Rules.new(:accept => [/1/])
+
+    rules.accept?('1').should == true
+  end
+
+  it "should match non-Strings using acceptance regexps" do
+    rules = Rules.new(:accept => [/1/])
+
+    rules.accept?(1).should == true
+  end
+
+  it "should accept data using acceptance lambdas" do
+    rules = Rules.new(:accept => [lambda { |data| data > 2 }])
+
+    rules.accept?(3).should == true
+  end
+
+  it "should reject data that does not match any acceptance rules" do
+    rules = Rules.new(:accept => [1, 2, 3])
+
+    rules.accept?(2).should == true
+    rules.accept?(4).should == false
+  end
+
+  it "should accept data that does not match any rejection rules" do
+    rules = Rules.new(:reject => [1, 2, 3])
+
+    rules.accept?(2).should == false
+    rules.accept?(4).should == true
+  end
+end
