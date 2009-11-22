6abe4f55676533525a30ab0cf4fcbc27e4975174
postmodern <postmodern.mod3@gmail.com>
2009-11-22 05:56:24 -0800

    Added convenience methods for wraping around every_page.

--- lib/spidr/events.rb
+++ lib/spidr/events.rb
@@ -57,6 +57,20 @@ module Spidr
     end
 
     #
+    # Pass the headers from every response the agent receives to a given
+    # block.
+    #
+    # @yield [headers]
+    #   The block will be passed the headers of every response.
+    #
+    # @yieldparam [Hash] headers
+    #   The headers from a response.
+    #
+    def all_headers(&block)
+      every_page { |page| block.call(page.headers) }
+    end
+
+    #
     # Pass every page that the agent visits to a given block.
     #
     # @yield [page]
@@ -71,17 +85,273 @@ module Spidr
     end
 
     #
-    # Pass the headers from every response the agent receives to a given
-    # block.
+    # Pass every OK page that the agent visits to a given block.
     #
-    # @yield [headers]
-    #   The block will be passed the headers of every response.
+    # @yield [page]
+    #   The block will be passed every OK page visited.
     #
-    # @yieldparam [Hash] headers
-    #   The headers from a response.
+    # @yieldparam [Page] page
+    #   A visited page.
     #
-    def all_headers(&block)
-      every_page { |page| block.call(page.headers) }
+    def every_ok_page(&block)
+      every_page do |page|
+        block.call(page) if (block && page.ok?)
+      end
+    end
+
+    #
+    # Pass every Redirect page that the agent visits to a given block.
+    #
+    # @yield [page]
+    #   The block will be passed every Redirect page visited.
+    #
+    # @yieldparam [Page] page
+    #   A visited page.
+    #
+    def every_redirect_page(&block)
+      every_page do |page|
+        block.call(page) if (block && page.redirect?)
+      end
+    end
+
+    #
+    # Pass every Timeout page that the agent visits to a given block.
+    #
+    # @yield [page]
+    #   The block will be passed every Timeout page visited.
+    #
+    # @yieldparam [Page] page
+    #   A visited page.
+    #
+    def every_timedout_page(&block)
+      every_page do |page|
+        block.call(page) if (block && page.timedout?)
+      end
+    end
+
+    #
+    # Pass every Bad Request page that the agent visits to a given block.
+    #
+    # @yield [page]
+    #   The block will be passed every Bad Request page visited.
+    #
+    # @yieldparam [Page] page
+    #   A visited page.
+    #
+    def every_bad_request_page(&block)
+      every_page do |page|
+        block.call(page) if (block && page.bad_request?)
+      end
+    end
+
+    #
+    # Pass every Unauthorized page that the agent visits to a given block.
+    #
+    # @yield [page]
+    #   The block will be passed every Unauthorized page visited.
+    #
+    # @yieldparam [Page] page
+    #   A visited page.
+    #
+    def every_unauthorized_page(&block)
+      every_page do |page|
+        block.call(page) if (block && page.unauthorized?)
+      end
+    end
+
+    #
+    # Pass every Forbidden page that the agent visits to a given block.
+    #
+    # @yield [page]
+    #   The block will be passed every Forbidden page visited.
+    #
+    # @yieldparam [Page] page
+    #   A visited page.
+    #
+    def every_forbidden_page(&block)
+      every_page do |page|
+        block.call(page) if (block && page.forbidden?)
+      end
+    end
+
+    #
+    # Pass every Missing page that the agent visits to a given block.
+    #
+    # @yield [page]
+    #   The block will be passed every Missing page visited.
+    #
+    # @yieldparam [Page] page
+    #   A visited page.
+    #
+    def every_missing_page(&block)
+      every_page do |page|
+        block.call(page) if (block && page.missing?)
+      end
+    end
+
+    #
+    # Pass every Internal Server Error page that the agent visits to a given block.
+    #
+    # @yield [page]
+    #   The block will be passed every Internal Server Error page visited.
+    #
+    # @yieldparam [Page] page
+    #   A visited page.
+    #
+    def every_internal_server_error_page(&block)
+      every_page do |page|
+        block.call(page) if (block && page.had_internal_server_error?)
+      end
+    end
+
+    #
+    # Pass every Plain Text page that the agent visits to a given block.
+    #
+    # @yield [page]
+    #   The block will be passed every Plain Text page visited.
+    #
+    # @yieldparam [Page] page
+    #   A visited page.
+    #
+    def every_txt_page(&block)
+      every_page do |page|
+        block.call(page) if (block && page.txt?)
+      end
+    end
+
+    #
+    # Pass every HTML page that the agent visits to a given block.
+    #
+    # @yield [page]
+    #   The block will be passed every HTML page visited.
+    #
+    # @yieldparam [Page] page
+    #   A visited page.
+    #
+    def every_html_page(&block)
+      every_page do |page|
+        block.call(page) if (block && page.html?)
+      end
+    end
+
+    #
+    # Pass every XML page that the agent visits to a given block.
+    #
+    # @yield [page]
+    #   The block will be passed every XML page visited.
+    #
+    # @yieldparam [Page] page
+    #   A visited page.
+    #
+    def every_xml_page(&block)
+      every_page do |page|
+        block.call(page) if (block && page.xml?)
+      end
+    end
+
+    #
+    # Pass every JavaScript page that the agent visits to a given block.
+    #
+    # @yield [page]
+    #   The block will be passed every JavaScript page visited.
+    #
+    # @yieldparam [Page] page
+    #   A visited page.
+    #
+    def every_javascript_page(&block)
+      every_page do |page|
+        block.call(page) if (block && page.javascript?)
+      end
+    end
+
+    #
+    # Pass every CSS page that the agent visits to a given block.
+    #
+    # @yield [page]
+    #   The block will be passed every CSS page visited.
+    #
+    # @yieldparam [Page] page
+    #   A visited page.
+    #
+    def every_css_page(&block)
+      every_page do |page|
+        block.call(page) if (block && page.css?)
+      end
+    end
+
+    #
+    # Pass every RSS feed that the agent visits to a given block.
+    #
+    # @yield [feed]
+    #   The block will be passed every RSS feed visited.
+    #
+    # @yieldparam [Page] feed
+    #   A visited page.
+    #
+    def every_rss_page(&block)
+      every_page do |page|
+        block.call(page) if (block && page.rss?)
+      end
+    end
+
+    #
+    # Pass every Atom feed that the agent visits to a given block.
+    #
+    # @yield [feed]
+    #   The block will be passed every Atom feed visited.
+    #
+    # @yieldparam [Page] feed
+    #   A visited page.
+    #
+    def every_atom_page(&block)
+      every_page do |page|
+        block.call(page) if (block && page.atom?)
+      end
+    end
+
+    #
+    # Pass every MS Word page that the agent visits to a given block.
+    #
+    # @yield [page]
+    #   The block will be passed every MS Word page visited.
+    #
+    # @yieldparam [Page] page
+    #   A visited page.
+    #
+    def every_ms_word_page(&block)
+      every_page do |page|
+        block.call(page) if (block && page.ms_word?)
+      end
+    end
+
+    #
+    # Pass every PDF page that the agent visits to a given block.
+    #
+    # @yield [page]
+    #   The block will be passed every PDF page visited.
+    #
+    # @yieldparam [Page] page
+    #   A visited page.
+    #
+    def every_pdf_page(&block)
+      every_page do |page|
+        block.call(page) if (block && page.pdf?)
+      end
+    end
+
+    #
+    # Pass every ZIP page that the agent visits to a given block.
+    #
+    # @yield [page]
+    #   The block will be passed every ZIP page visited.
+    #
+    # @yieldparam [Page] page
+    #   A visited page.
+    #
+    def every_zip_page(&block)
+      every_page do |page|
+        block.call(page) if (block && page.zip?)
+      end
     end
   end
 end
