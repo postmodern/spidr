38d72b87eae58e0a576cd6dbb8d10a3bb6b0469a
postmodern <postmodern.mod3@gmail.com>
2009-12-31 20:08:22 -0800

    Fixed a Page spec.

--- spec/page_examples.rb
+++ spec/page_examples.rb
@@ -12,6 +12,6 @@ shared_examples_for "Page" do
   end
 
   it "should provide transparent access to the response headers" do
-    @page.content_type.should == @page.content_type
+    @page.content_type.should == @page.response['content-type']
   end
 end
