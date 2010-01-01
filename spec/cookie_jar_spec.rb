292cf2b5d6530f927ad62acaed5b09a0a420291b
postmodern <postmodern.mod3@gmail.com>
2009-12-31 22:25:17 -0800

    Added another spec for the CookieJar.

--- spec/cookie_jar_spec.rb
+++ spec/cookie_jar_spec.rb
@@ -7,19 +7,26 @@ describe CookieJar do
     @cookie_jar = CookieJar.new
   end
 
-  it 'should add a cookie to the jar' do
+  it "should add a cookie to the jar" do
     @cookie_jar['zerosum.org'] = 'admin=ofcourseiam'
 
     @cookie_jar.size.should == 1
   end
 
-  it 'should retrieve cookies for the named host' do
+  it "should override previous cookies in the jar" do
+    @cookie_jar['zerosum.org'] = 'admin=ofcourseiam'
+    @cookie_jar['zerosum.org'] = 'admin=somethingcompletelydifferent'
+
+    @cookie_jar['zerosum.org'].should == 'admin=somethingcompletelydifferent'
+  end
+
+  it "should retrieve cookies for the named host" do
     @cookie_jar['zerosum.org'] = 'admin=ofcourseiam'
 
     @cookie_jar['zerosum.org'].should == 'admin=ofcourseiam'
   end
 
-  it 'should clear all cookies' do
+  it "should clear all cookies" do
     @cookie_jar['zerosum.org'] = 'cookie=foobar'
     @cookie_jar.clear!
 
