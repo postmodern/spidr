2fec2e86c427ccffcf67d81e1c2d9e6476328e50
postmodern <postmodern.mod3@gmail.com>
2010-02-27 17:01:38 -0800

    Added basic specs for Spidr::SessionCache.

---
+++ spec/session_cache.rb
@@ -0,0 +1,50 @@
+require 'spidr/session_cache'
+
+require 'spec_helper'
+
+describe SessionCache do
+  describe "empty" do
+    before(:all) do
+      @sessions = SessionCache.new
+    end
+
+    it "should not have any active sessions" do
+      @sessions.should_not be_active(URI('http://example.com/'))
+    end
+
+    it "should start new sessions on-demand" do
+      @sessions[URI('http://example.com/')].should_not be_nil
+    end
+
+    after(:all) do
+      @sessions.clear
+    end
+  end
+
+  describe "not-empty" do
+    before(:all) do
+      @url = URI('http://example.com/')
+
+      @sessions = SessionCache.new
+      @sessions[@url]
+    end
+
+    it "should have active sessions" do
+      @sessions.should be_active(@url)
+    end
+
+    it "should provide access to sessions" do
+      @sessions[@url].should_not be_nil
+    end
+
+    it "should start new sessions on-demand" do
+      url2 = URI('http://www.w3c.org/')
+
+      @sessions[url2].should_not be_nil
+    end
+
+    after(:all) do
+      @sessions.clear
+    end
+  end
+end
