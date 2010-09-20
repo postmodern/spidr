3dadd085337bfbdabdc0334e38f4bc5214eb8b72
Postmodern <postmodern.mod3@gmail.com>
2010-09-20 03:05:56 -0700

    No longer activate the runtime group.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'bundler'
 
 begin
-  Bundler.setup(:runtime, :test)
+  Bundler.setup(:test)
 rescue Bundler::BundlerError => e
   STDERR.puts e.message
   STDERR.puts "Run `bundle install` to install missing gems"
