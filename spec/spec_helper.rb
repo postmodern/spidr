d5465fce72bfbb3b4c8ac037c55de67d8ed4a155
Postmodern <postmodern.mod3@gmail.com>
2010-10-10 19:47:11 -0700

    RSpec 2.0.0 supports activating Bundler.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,3 @@
-require 'rubygems'
-require 'bundler'
-
-begin
-  Bundler.setup(:test)
-rescue Bundler::BundlerError => e
-  STDERR.puts e.message
-  STDERR.puts "Run `bundle install` to install missing gems"
-  exit e.status_code
-end
-
 require 'rspec'
 require 'spidr/version'
 
