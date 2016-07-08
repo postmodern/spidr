e4dae3673273b91a4c1a121e63eeeea1c7cd7647
Postmodern <postmodern.mod3@gmail.com>
2016-07-08 16:16:55 -0700

    Should be in the Settings namespace.

--- lib/spidr/settings/timeouts.rb
+++ lib/spidr/settings/timeouts.rb
@@ -1,31 +1,33 @@
 module Spidr
-  #
-  # @since 0.6.0
-  #
-  module Timeouts
-    # Read timeout.
+  module Settings
     #
-    # @return [Integer, nil]
-    attr_accessor :read_timeout
+    # @since 0.6.0
+    #
+    module Timeouts
+      # Read timeout.
+      #
+      # @return [Integer, nil]
+      attr_accessor :read_timeout
 
-    # Open timeout.
-    #
-    # @return [Integer, nil]
-    attr_accessor :open_timeout
+      # Open timeout.
+      #
+      # @return [Integer, nil]
+      attr_accessor :open_timeout
 
-    # SSL timeout.
-    #
-    # @return [Integer, nil]
-    attr_accessor :ssl_timeout
+      # SSL timeout.
+      #
+      # @return [Integer, nil]
+      attr_accessor :ssl_timeout
 
-    # `Continue` timeout.
-    #
-    # @return [Integer, nil]
-    attr_accessor :continue_timeout
+      # `Continue` timeout.
+      #
+      # @return [Integer, nil]
+      attr_accessor :continue_timeout
 
-    # `Keep-Alive` timeout.
-    #
-    # @return [Integer, nil]
-    attr_accessor :keep_alive_timeout
+      # `Keep-Alive` timeout.
+      #
+      # @return [Integer, nil]
+      attr_accessor :keep_alive_timeout
+    end
   end
 end
