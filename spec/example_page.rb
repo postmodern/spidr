f8620457b68a51483456dd3667466fec54594f84
Postmodern <postmodern.mod3@gmail.com>
2016-08-03 18:02:17 -0700

    Forgot to commit the example Page context.

---
+++ spec/example_page.rb
@@ -0,0 +1,29 @@
+require 'rspec'
+
+RSpec.shared_context "example Page" do
+  let(:code)          { 200 }
+  let(:msg)           { 'OK'  }
+  let(:content_type)  { 'text/html' }
+  let(:headers)       { {} }
+  let(:body)          { '' }
+
+  let(:response) do
+    Net::HTTPResponse.new('1.1', code.to_s, msg).tap do |response|
+      response.set_content_type(content_type) if content_type
+
+      headers.each do |name,value|
+        if value then response.add_field(name,value)
+        else          response.remove_field(name)
+        end
+      end
+
+      # stub #body, otherwise Net::HTTP will check @socket
+      allow(response).to receive(:body).and_return(body)
+    end
+  end
+
+  let(:host) { 'example.com' }
+  let(:url)  { URI::HTTP.build(host: host) }
+
+  subject { described_class.new(url,response) }
+end
