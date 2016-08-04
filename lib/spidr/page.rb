9890530b1bd5d94ec1f074934fc503e4cd9c186a
Postmodern <postmodern.mod3@gmail.com>
2016-08-03 18:02:03 -0700

    Re-arranged Page methods.

--- lib/spidr/page.rb
+++ lib/spidr/page.rb
@@ -1,8 +1,3 @@
-require 'spidr/page/status_codes'
-require 'spidr/page/content_types'
-require 'spidr/page/cookies'
-require 'spidr/page/html'
-
 module Spidr
   #
   # Represents a requested page from a website.
@@ -35,21 +30,87 @@ module Spidr
     end
 
     #
-    # The meta-redirect links of the page.
+    # The body of the response.
     #
-    # @return [Array<String>]
-    #   All meta-redirect links in the page.
+    # @return [String]
+    #   The body of the response.
     #
-    # @deprecated
-    #   Deprecated in 0.3.0 and will be removed in 0.4.0.
-    #   Use {#meta_redirects} instead.
-    #
-    def meta_redirect
-      warn 'DEPRECATION: Spidr::Page#meta_redirect will be removed in 0.3.0'
-      warn 'DEPRECATION: Use Spidr::Page#meta_redirects instead'
-
-      meta_redirects
+    def body
+      (response.body || '')
     end
+
+    #
+    # Returns a parsed document object for HTML, XML, RSS and Atom pages.
+    #
+    # @return [Nokogiri::HTML::Document, Nokogiri::XML::Document, nil]
+    #   The document that represents HTML or XML pages.
+    #   Returns `nil` if the page is neither HTML, XML, RSS, Atom or if
+    #   the page could not be parsed properly.
+    #
+    # @see http://nokogiri.rubyforge.org/nokogiri/Nokogiri/XML/Document.html
+    # @see http://nokogiri.rubyforge.org/nokogiri/Nokogiri/HTML/Document.html
+    #
+    def doc
+      unless body.empty?
+        doc_class = if html?
+                      Nokogiri::HTML::Document
+                    elsif rss? || atom? || xml? || xsl?
+                      Nokogiri::XML::Document
+                    end
+
+        if doc_class
+          begin
+            @doc ||= doc_class.parse(body, @url.to_s, content_charset)
+          rescue
+          end
+        end
+      end
+    end
+
+    #
+    # Searches the document for XPath or CSS Path paths.
+    #
+    # @param [Array<String>] paths
+    #   CSS or XPath expressions to search the document with.
+    #
+    # @return [Array]
+    #   The matched nodes from the document.
+    #   Returns an empty Array if no nodes were matched, or if the page
+    #   is not an HTML or XML document.
+    #
+    # @example
+    #   page.search('//a[@href]')
+    #
+    # @see http://nokogiri.rubyforge.org/nokogiri/Nokogiri/XML/Node.html#M000239
+    #
+    def search(*paths)
+      if doc
+        doc.search(*paths)
+      else
+        []
+      end
+    end
+
+    #
+    # Searches for the first occurrence an XPath or CSS Path expression.
+    #
+    # @return [Nokogiri::HTML::Node, Nokogiri::XML::Node, nil]
+    #   The first matched node. Returns `nil` if no nodes could be matched,
+    #   or if the page is not a HTML or XML document.
+    #
+    # @example
+    #   page.at('//title')
+    #
+    # @see http://nokogiri.rubyforge.org/nokogiri/Nokogiri/XML/Node.html#M000251
+    #
+    def at(*arguments)
+      if doc
+        doc.at(*arguments)
+      end
+    end
+
+    alias / search
+    alias % at
 
     protected
 
@@ -82,3 +143,8 @@ module Spidr
   
   end
 end
+
+require 'spidr/page/status_codes'
+require 'spidr/page/content_types'
+require 'spidr/page/cookies'
+require 'spidr/page/html'
--- spec/page_spec.rb
+++ spec/page_spec.rb
@@ -46,4 +46,114 @@ describe Page do
       end
     end
   end
+
+  describe "#body" do
+    context "when there is a body" do
+      let(:body) { %{<html><head><title>example</title></head><body><p>hello</p></body></html>} }
+
+      it "should return the body text" do
+        expect(subject.body).to be body
+      end
+    end
+
+    context "when there is no body" do
+      it "should return an empty String" do
+        expect(subject.body).to be == ''
+      end
+    end
+  end
+
+  describe "#doc" do
+    context "when the Content-Type is text/html" do
+      let(:body) { %{<html><head><title>example</title></head><body><p>hello</p></body></html>} }
+
+      it "should parse the body as HTML" do
+        expect(subject.doc).to be_kind_of(Nokogiri::HTML::Document)
+        expect(subject.doc.at('//p').inner_text).to be == 'hello'
+      end
+    end
+
+    context "when the document is application/rss+xml" do
+      let(:content_type) { 'application/rss+xml' }
+      let(:body) do
+        %{<?xml version="1.0" encoding="UTF-8" ?><rss version="2.0"></rss>}
+      end
+
+      it "should parse the body as XML" do
+        expect(subject.doc).to be_kind_of(Nokogiri::XML::Document)
+      end
+    end
+
+    context "when the document is application/atom+xml" do
+      let(:content_type) { 'application/atom+xml' }
+      let(:body) do
+        %{<?xml version="1.0" encoding="UTF-8" ?><feed xmlns="http://www.w3.org/2005/Atom"></feed>}
+      end
+
+      it "should parse the body as XML" do
+        expect(subject.doc).to be_kind_of(Nokogiri::XML::Document)
+      end
+    end
+
+    context "when the document is text/xml" do
+      let(:content_type) { 'text/xml' }
+      let(:body) do
+        %{<?xml version="1.0" encoding="UTF-8" ?><foo />}
+      end
+
+      it "should parse the body as XML" do
+        expect(subject.doc).to be_kind_of(Nokogiri::XML::Document)
+      end
+    end
+
+    context "when the document is text/xsl" do
+      let(:content_type) { 'text/xsl' }
+      let(:body) do
+        %{<?xml version="1.0" encoding="UTF-8" ?><xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"></xsl:stylesheet>}
+      end
+
+      it "should parse the body as XML" do
+        expect(subject.doc).to be_kind_of(Nokogiri::XML::Document)
+      end
+    end
+
+    context "when there is no body" do
+      it "should return an empty String" do
+        expect(subject.doc).to be nil
+      end
+    end
+  end
+
+  describe "#search" do
+    context "when there is a document" do
+      let(:body) { %{<html><head><title>example</title></head><body><p>hello</p></body></html>} }
+
+      it "should search the document" do
+        expect(subject.search('//p').inner_text).to be == 'hello'
+      end
+    end
+
+    context "when there is no document" do
+      it "should return an empty Array" do
+        expect(subject.search('//p')).to be == []
+      end
+    end
+  end
+
+  describe "#at" do
+    context "when there is a document" do
+      let(:body) { %{<html><head><title>example</title></head><body><p>hello</p></body></html>} }
+
+      it "should search the document for the first matching node" do
+        expect(subject.at('//p').inner_text).to be == 'hello'
+      end
+    end
+
+    context "when there is no document" do
+      it "should return nil" do
+        expect(subject.at('//p')).to be nil
+      end
+    end
+  end
+
 end
