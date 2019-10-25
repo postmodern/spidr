29c53628de5ac17091f6229dcb1609feec76c331
Postmodern <postmodern.mod3@gmail.com>
2019-10-24 20:21:38 -0700

    Revert "Hack solution until https://github.com/bblimke/webmock/issues/642 is resolved"
    This reverts commit 496f3f948040bc17e700ce9c5bfc45b38dcda2ea.

--- spec/agent_spec.rb
+++ spec/agent_spec.rb
@@ -760,11 +760,6 @@ describe Agent do
   end
 
   context "when :robots is enabled" do
-    # TODO: Delete this hack once https://github.com/bblimke/webmock/issues/642 is resolved
-    require 'webrick/httpstatus'
-    rack_response_filename = $LOADED_FEATURES.find { |filename| filename =~ /rack_response/ }
-    eval File.read(rack_response_filename).sub(/status$/, "[status, WEBrick::HTTPStatus.reason_phrase(status)]")
-
     include_context "example App"
 
     let(:user_agent) { 'Ruby' }
@@ -791,12 +786,14 @@ describe Agent do
 
         [
           "User-agent: *",
-          'Disallow: /secret',
+          'Disallow: /',
         ].join($/)
       end
     end
 
     it "should not follow links Disallowed by robots.txt" do
+      pending "https://github.com/bblimke/webmock/issues/642"
+
       expect(subject.history).to be == Set[
         URI("http://#{host}/"),
         URI("http://#{host}/pub")
