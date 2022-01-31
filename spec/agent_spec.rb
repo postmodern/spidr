e5f484e93ece8c3bef8d6d15ffbc799fc818ef2d
Postmodern <postmodern.mod3@gmail.com>
2022-01-31 08:50:17 -0800

    Add missing specs for `Agent.start_at`, `.site`, and `.host` (closes #77, #78, #79).

--- spec/agent_spec.rb
+++ spec/agent_spec.rb
@@ -7,6 +7,183 @@ require 'spidr/agent'
 describe Agent do
   it_should_behave_like "includes Spidr::Settings::UserAgent"
 
+  describe ".start_at" do
+    module TestAgentStartAt
+      class ExampleApp < Sinatra::Base
+
+        set :host, 'example.com'
+        set :port, 80
+
+        get '/' do
+          '<html><body>should not get here</body></html>'
+        end
+
+        get '/entry-point' do
+          <<~HTML
+          <html>
+            <body>
+              <a href="/link1">link1</a>
+              <a href="http://other.com/offsite-link">offsite link</a>
+              <a href="/link2">link2</a>
+            </body>
+          </html>
+          HTML
+        end
+
+        get '/link1' do
+          '<html><body>got here</body></html>'
+        end
+
+        get '/link2' do
+          '<html><body>got here</body></html>'
+        end
+      end
+
+      class OtherApp < Sinatra::Base
+
+        set :host, 'other.com'
+        set :port, 80
+
+        get '/offsite-link' do
+          '<html><body>should not get here</body></html>'
+        end
+
+      end
+    end
+
+    subject { described_class }
+
+    let(:host)       { 'example.com' }
+    let(:other_host) { 'other.com'   }
+    let(:url)        { URI("http://#{host}/entry-point") }
+
+    let(:app)       { TestAgentStartAt::ExampleApp }
+    let(:other_app) { TestAgentStartAt::OtherApp   }
+
+    before do
+      stub_request(:any, /#{Regexp.escape(host)}/).to_rack(app)
+      stub_request(:any, /#{Regexp.escape(other_host)}/).to_rack(other_app)
+    end
+
+    it "must spider the website starting at the given URL" do
+      agent = subject.start_at(url)
+
+      expect(agent.history).to be == Set[
+        URI("http://#{host}/entry-point"),
+        URI("http://#{host}/link1"),
+        URI("http://#{other_host}/offsite-link"),
+        URI("http://#{host}/link2")
+      ]
+    end
+  end
+
+  describe ".site" do
+    module TestAgentSite
+      class ExampleApp < Sinatra::Base
+
+        set :host, 'example.com'
+        set :port, 80
+
+        get '/' do
+          '<html><body>should not get here</body></html>'
+        end
+
+        get '/entry-point' do
+          <<~HTML
+            <html>
+              <body>
+                <a href="/link1">link1</a>
+                <a href="http://other.com/offsite-link">offsite link</a>
+                <a href="/link2">link2</a>
+              </body>
+            </html>
+          HTML
+        end
+
+        get '/link1' do
+          '<html><body>got here</body></html>'
+        end
+
+        get '/link2' do
+          '<html><body>got here</body></html>'
+        end
+
+      end
+    end
+
+    subject { described_class }
+
+    let(:host) { 'example.com' }
+    let(:url)  { URI("http://#{host}/entry-point") }
+
+    let(:app) { TestAgentSite::ExampleApp }
+
+    before do
+      stub_request(:any, /#{Regexp.escape(host)}/).to_rack(app)
+    end
+
+    it "must spider the website starting at the given URL" do
+      agent = subject.site(url)
+
+      expect(agent.history).to be == Set[
+        URI("http://#{host}/entry-point"),
+        URI("http://#{host}/link1"),
+        URI("http://#{host}/link2")
+      ]
+    end
+  end
+
+  describe ".host" do
+    module TestAgentHost
+      class ExampleApp < Sinatra::Base
+
+        set :host, 'example.com'
+        set :port, 80
+
+        get '/' do
+          <<~HTML
+            <html>
+              <body>
+                <a href="/link1">link1</a>
+                <a href="http://other.com/offsite-link">offsite link</a>
+                <a href="/link2">link2</a>
+              </body>
+            </html>
+          HTML
+        end
+
+        get '/link1' do
+          '<html><body>got here</body></html>'
+        end
+
+        get '/link2' do
+          '<html><body>got here</body></html>'
+        end
+
+      end
+    end
+
+    subject { described_class }
+
+    let(:host) { 'example.com' }
+    let(:app)  { TestAgentHost::ExampleApp }
+
+    before do
+      stub_request(:any, /#{Regexp.escape(host)}/).to_rack(app)
+    end
+
+    it "must spider the website starting at the given URL" do
+      agent = subject.host(host)
+
+      # XXX: for some reason Set#== was returning false, so convert to an Array
+      expect(agent.history.to_a).to be == [
+        URI("http://#{host}/"),
+        URI("http://#{host}/link1"),
+        URI("http://#{host}/link2")
+      ]
+    end
+  end
+
   describe "#initialize" do
     it "should not be running" do
       expect(subject).to_not be_running
